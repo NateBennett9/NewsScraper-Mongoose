05db164afa2100f42fe53d4d1fa39de556448d2a
Nate Bennett <26212562+NateBennett9@users.noreply.github.com>
2019-08-01 17:02:12 -0700

    heroku deploy

--- server.js
+++ server.js
@@ -14,27 +14,28 @@ require("./config/routes")(router);
 app.use(express.static(__dirname + "/public"));
 
 app.engine("handlebars", expressHandlebars({
-    defaultLayout: "main"
+  defaultLayout: "main"
 }));
 app.set("view engine", "handlebars");
 
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
 
 app.use(router);
 
+
 var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(db, function (error) {
-    if (error) {
-        console.log(error);
-    }
-    else {
-        console.log("mongoose connection is successful");
-    }
+mongoose.connect(db, function(error) {
+  if (error) {
+    console.log(error);
+  }
+  else {
+    console.log("mongoose connection is successful");
+  }
 });
 
-app.listen(PORT, function () {
-    console.log("Listening on port:" + PORT);
+app.listen(PORT, function() {
+  console.log("Listening on port:" + PORT);
 });
